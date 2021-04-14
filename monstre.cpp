123d376f6f959149a5397a2a2bf2e1a7642745fe
BalcouEwennETPA <ewennbalcou10@gmail.com>
2021-04-14 14:58:50 +0200

    Update monstre.cpp

--- monstre.cpp
+++ monstre.cpp
@@ -9,3 +9,10 @@ Monstre::Monstre() : name("Gobelin"), att(2), hp(3), currentState(true){
 Monstre::Monstre(std::string nom, int pv, int degats, bool etat) : name(nom), hp(pv), att(degats), currentState(etat){
 
 }
+
+void Monstre::Affichage(){
+    cout << name << " : Attaque = " << att << " HP = " << hp << endl;
+    if (hp == 0){
+        cout << name << " est mort " << endl ;
+    }
+}
